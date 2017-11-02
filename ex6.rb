f4e52f8ee8885091f2c312651dda3bded7121f2e
Dev (alt) <dev@173-168-100-37.res.bhn.net>
2017-11-02 18:43:02 +0530

    Exercise 6 string and text

---
+++ ex6.rb
@@ -0,0 +1,24 @@
+types_of_people = 10
+#types of people
+x = "Types of people #{types_of_people}"
+binary = 'binary'
+do_not = "don't"
+#some binary 
+y = "some knows #{binary} and some #{do_not}."
+
+#print strings with variables x and y
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: #{y}."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#print string 
+puts joke_evaluation
+
+w = 'This is the left side of...'
+e = 'a string with a right side.'
+#concatinating string 
+puts w + e
