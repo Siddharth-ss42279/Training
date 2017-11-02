98cdc0eddfbe75a79610f39b177b11671c9029f5
Dev (alt) <dev@Mini01s-Mac-mini.local>
2017-11-02 18:09:04 +0530

    exercise 5 conversion

--- ex5.rb
+++ ex5.rb
@@ -2,9 +2,11 @@ my_name = 'siddharth'
 
 my_age = 21 #siddharths age
 
-my_height = 74 # inches
+my_height_inches = 74 # inches
 
-my_weight = 180 #lbs
+my_height_cm = my_height_inches * 2.54
+my_weight_pound = 180 #pound
+my_weight_kg = my_weight_pound * 0.453592 
 
 my_eyes = 'Blue'
 
@@ -14,11 +16,11 @@ my_hair = 'Brown'
 
 
 puts "i am #{my_name}"
-puts "my height #{my_height}"
-puts "my weight #{my_weight}"
+puts "my height in centimeters #{my_height_cm} and inches #{my_height_inches} "
+puts "my weight in pound #{my_weight_pound} and kilogram #{my_weight_kg}"
 puts "my eyes and hair  #{my_eyes} and #{my_hair}"
 puts "my teeth #{my_teeth}"
 
 
 
-puts "age + height + weight = #{my_age + my_height + my_weight}"
+#puts "age + height + weight = #{my_age + my_height + my_weight}"
