0249fc0ea31dd75ebc5b431221e7c76a079e7ff0
Dev (alt) <dev@173-168-100-37.res.bhn.net>
2017-11-02 19:00:04 +0530

    Exercise 7 put and print

---
+++ ex7.rb
@@ -0,0 +1,19 @@
+puts "Its fleece was white as #{'snow'}."
+#print 10 dots in line
+puts "." * 10  # prints 10 dots consecutively
+#variable declaration 
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+#prints means same line and puts print in next line
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
