d4456f9e1ea36e0666419b46887e271c5eb8454c
Dev (alt) <dev@Mini01s-Mac-mini.local>
2017-11-02 17:03:55 +0530

    floating points excercise 3

--- ex3.rb
+++ ex3.rb
@@ -1,6 +1,6 @@
 puts "I will now count my chickens: "
 
-puts "Hens #{25 + 30 / 6} "
+puts "Hens #{25.4 + 30 / 6} "
 
 puts "Roosters #{100 - 25 * 3 % 4}"
 
@@ -20,4 +20,4 @@ puts "Is it greater? #{5 > -2}"
 
 puts "Is it greater or equal? #{5 >= -2}"
 
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it less or equal? #{5.600 <= -2}"
