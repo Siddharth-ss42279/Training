82995bdccb6969d57d6817de7d339f95128fb7ce
Dev (alt) <dev@Mini01s-Mac-mini.local>
2017-11-02 17:51:17 +0530

    excercise 5 variables and printing

---
+++ ex5.rb
@@ -0,0 +1,24 @@
+my_name = 'siddharth'
+
+my_age = 21 #siddharths age
+
+my_height = 74 # inches
+
+my_weight = 180 #lbs
+
+my_eyes = 'Blue'
+
+my_teeth = 'white'
+
+my_hair = 'Brown'
+
+
+puts "i am #{my_name}"
+puts "my height #{my_height}"
+puts "my weight #{my_weight}"
+puts "my eyes and hair  #{my_eyes} and #{my_hair}"
+puts "my teeth #{my_teeth}"
+
+
+
+puts "age + height + weight = #{my_age + my_height + my_weight}"
